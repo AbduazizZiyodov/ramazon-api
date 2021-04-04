065ecebde8b895c27a0e6f0fedf535ea9ba286b9
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-04-04 16:59:57 +0500

    :hammer: added some helper functions

---
+++ helpers.py
@@ -0,0 +1,23 @@
+from datetime import date
+from fastapi import FastAPI
+from fastapi import HTTPException
+from tortoise.contrib.fastapi import register_tortoise
+
+
+
+today = date.today().strftime("%Y-%m-%d")
+
+
+def configure_db(app: FastAPI):
+    register_tortoise(
+        app,
+        db_url="sqlite://db.sqlite3",
+        modules={'models': ['database.models']},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+
+
+def http_404():
+    raise HTTPException(
+        status_code=404, detail="Ma'lumot Topilmadi")
