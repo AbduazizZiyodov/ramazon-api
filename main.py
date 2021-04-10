4ed1c202639f2ae5afee1e34bf2dd0afd872a234
AbduazizZiyodov <abduaziz.ziyodov@mail.ru>
2021-04-10 22:01:28 +0500

    ↩️  application structure changed

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 # -------------------------------------------------------- #
+
 from helpers import current
 from helpers import metadata
 from helpers import http_404
 from helpers import configure_db
-from database.models import Date
-from database.models import Region
+from core.dates import dates
+from core.regions import regions
 
 
 api = FastAPI(
     title="RamazonAPI",
     openapi_tags=metadata,
     version="2.0.0",
-    description=">**API** for Ramadan Calendar(2021). Author: Abduaziz Ziyodov",
+    description="**API** for Ramadan Calendar(2021). <br> Assalamu alaikum👋 Ramadan Mubarak. Wishing you a blessed and Happy Ramadan! <br> Data about times in this month is extremely important. <br> That's why I transferred this information to the **API** interface. In my opinion, this **API** will benefit everyone. <br> **Author: Abduaziz Ziyodov**",
     openapi_url="/api/v2/openapi.json",
-    redoc_url=None,
+    redoc_url=None
 )
 
+api.include_router(regions.router)
+api.include_router(dates.router)
 
 api.add_middleware(
     CORSMiddleware,
@@ -35,104 +38,9 @@ configure_db(app=api)
 
 @api.get('/', tags=["Basic Route"])
 async def home_page():
-    return JSONResponse({
+    return {
         "muvaffaqiyat": True,
         "Sarlavha": "Ramazon-API",
         "Muallif": "Abduaziz Ziyodov",
         "GitHub": "https://github.com/AbduazizZiyodov/ramazon-api"
-    })
-
-
-@api.get('/api/v2/regions',
-        tags=["Regions"],
-        response_model=List[schemas.Region])
-async def get_regions():
-    return await Region.all()
-
-
-@api.get('/api/v2/regions/{id}',
-        tags=["Regions"],
-        response_model=schemas.Region)
-async def get_region_by_id(id: int):
-    data = Region.filter(hudud_id=id)
-    resp: Region = await data.get_or_none() or http_404()
-
-    return resp.full_format()
-
-
-@api.get('/api/v2/dates',
-        tags=["Dates"], 
-        response_model=List[schemas.Dates])
-async def get_dates():
-    regions = await Region.all()
-    response = []
-    for region in regions:
-        query = await Date.filter(hudud=region.hudud)
-        response.append({
-            "hudud": region.hudud,
-            "hudud_id": region.hudud_id,
-            "data": [q.full_format() for q in query]})
-    return response
-
-
-async def get_current(region: str):
-    data = await Date.filter(kun_full=current, hudud=region)\
-        .first()
-    if data is None:
-        return http_404()
-
-    return data.response_format()
-
-
-@api.get('/api/v2/dates/today', tags=["Dates"])
-async def get_dates_today():
-    query = await Region.all()
-    return [await get_current(region=region.hudud) for region in query]
-
-
-@api.get('/api/v2/regions/{id}/dates/today',
-        tags=["Dates"], 
-        response_model=schemas.Date)
-async def get_dates_today_by_region(id: int):
-    data = await Region.filter(hudud_id=id).first()
-    if data is None:
-        http_404()
-    today_data = await Date\
-        .filter(kun_full=current, hudud=data.hudud).first()
-
-    if today_data is None:
-        return http_404()
-
-    return today_data.full_format()
-
-
-@api.get('/api/v2/regions/{id}/dates',
-        tags=["Dates"],
-        response_model=List[schemas.Date])
-async def get_dates_by_region(id: int):
-    region = await Region.filter(hudud_id=id).first()
-
-    if region is None:
-        return http_404()
-
-    dates = await Date.filter(hudud=region.hudud)
-
-    return [d.full_format() for d in dates]
-
-
-@api.get('/api/v2/regions/{region_id}/day/{day}',
-        tags=["Dates"], 
-        response_model=schemas.Date)
-async def get_spec_data(region_id: int, day: int):
-    region = await Region.filter(hudud_id=region_id).first()
-
-    if region is None:
-        return http_404()
-
-    date = await Date.filter(hudud=region.hudud, kun=day)\
-                                                .first()
-
-    if date is None:
-        return http_404()
-
-    return date.full_format()
+    }
