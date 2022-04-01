bd53436e11f59435afe9a6c701d0629aa2fab439
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-04-02 01:38:04 +0500

    ✨ Define fastapi startup event

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,7 @@
+import sentry_sdk
+from os import getenv
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
 from api import settings
 from api.utils import setUp
@@ -13,7 +16,13 @@ routers = [dates_router, regions_router]
 if api.debug:
     routers.append(mock_router)
 
-setUp(api, routers)
+@api.on_event("startup")
+async def startup_event():
+    load_dotenv(), setUp(api, routers)
+    sentry_sdk.init(
+        getenv("SENTRY_DSN"),
+        traces_sample_rate=1.0
+    )
 
 
 @api.get("/", tags=["Base Route"])
